70e761e1fccab439162028967c980865e09b8329
mijailCR12 <66917852+mijailCR12@users.noreply.github.com>
2023-10-02 19:09:37 -0600

    jh

---
+++ src/components/AuthorDetails.vue
@@ -0,0 +1,99 @@
+<!-- authorDetails.vue -->
+<template>
+  <div class="row">
+   <div class="eleven column" style="margin-top: 5%">
+    <h2>{{title}}</h2>
+     <form>
+     <div class="row">
+      <div class="six columns">
+       <label for="authorInput">Author</label>
+       <input class="u-full-width" type="text"
+          v-model="author.author">
+      </div>
+     </div>
+     <div class="row">
+      <div class="six columns">
+       <label for="publisherInput">nationality</label>
+       <input class="u-full-width" type="text"
+          v-model="author.nationality">
+      </div>
+     </div>
+     <div class="row">
+      <div class="six columns">
+       <label for="copyrightInput">birth_year</label>
+       <input class="u-full-width" type="number"
+          v-model="author.copyright">
+      </div>
+      <div class="six columns">
+       <label for="languageInput">fields</label>
+       <input class="u-full-width" type="text"
+         v-model="author.language">
+      </div>
+     </div>
+     <div class="row">
+      <router-link class="button button-primary" 
+        to="/author">Back</router-link>
+       <a v-if='edit' class="button button-primary" style="float: right"
+         v-on:click="updateAuthor(author._id)">Update</a>
+       <a v-if='create' class="button button-primary" style="float: right"
+         v-on:click="createAuthor()">Create</a>
+     </div>
+    </form>
+  </div>
+</div>
+</template>
+
+<script>
+import { useRoute } from 'vue-router'
+
+export default {
+  name: "Author Details",
+  props: ['create','edit','create'],
+  data() {
+    return {
+      title: "Author Data",
+      author: {}
+    }
+  },
+  mounted() {
+    const route = useRoute()
+    if (route.params.id != null)
+      this.findAuthor(route.params.id);
+    else {
+      this.author = {
+        '_id': Math.floor(Math.random()*100000000),'author':'','nationality':'',
+        'birth_year':'','fields':''};
+    }
+  },
+  methods: {
+    findAuthor: function(id) {
+      fetch(this.url+'/.netlify/functions/authorFind/'+id,
+      { headers: {'Accept': 'application/json'}})
+      .then((response) => response.json())
+      .then((items) => {
+       this.author = items[0];
+      })
+    },
+    updateAuthor: function(id) {
+      fetch(this.url+'/.netlify/functions/authorUpdate/'+id,
+        { headers: {'Content-Type':'application/json'},
+          method: 'PUT',
+          body: JSON.stringify(this.author)})
+        .then((data) => {
+          this.$router.push('/author');
+        }
+      )
+    },
+    createAuthor: function() {
+      fetch(this.url+'/.netlify/functions/authorInsert',
+        { headers: {'Content-Type':'application/json'},
+          method: 'POST',
+          body: JSON.stringify(this.author)})
+        .then((data) => {
+           this.$router.push('/author');
+        }
+      )
+    }
+  }
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -3,6 +3,11 @@ import Home from "./components/Home.vue";
 import BookIndex from "./components/BookIndex.vue";
 import BookDetails from "./components/BookDetails.vue";
 
+import AuthorIndex from "./components/AuthorIndex.vue";
+import AuthorDetails from "./components/AuthorDetails.vue";
+
+import PublisherIndex from "./components/PublisherIndex.vue";
+import PublisherDetails from "./components/PublisherDetails.vue";
 const routes = [
   { path: "/", component: Home },
 	
@@ -15,6 +20,28 @@ const routes = [
     component: BookDetails, props: {create:true} },
   { path: "/book/delete/:id", 
     component: BookDetails, props: {delete:true} },
+
+  // authors
+  { path: "/author", component: AuthorIndex },
+  { path: "/author/show/:id", 
+    component: AuthorDetails, props: {show:true} },
+  { path: "/author/edit/:id", 
+    component: AuthorDetails, props: {edit:true} },
+  { path: "/author/create", 
+    component: AuthorDetails, props: {create:true} },
+  { path: "/author/delete/:id", 
+    component: AuthorDetails, props: {delete:true} },
+
+  // publisher
+  { path: "/publisher", component: PublisherIndex },
+  { path: "/publisher/show/:id", 
+    component: PublisherDetails, props: {show:true} },
+  { path: "/publisher/edit/:id", 
+    component: PublisherDetails, props: {edit:true} },
+  { path: "/publisher/create", 
+    component: PublisherDetails, props: {create:true} },
+  { path: "/publisher/delete/:id", 
+    component: PublisherDetails, props: {delete:true} },
 ];
 
 const history = createWebHistory();
